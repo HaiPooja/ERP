0ce29ca9d2612ce336f784ceb50e591a4b7d8601
HaiPooja <pooja@gmail.com>
2021-08-17 22:55:40 +0530

    adding oraganization option and search menu(modified)

--- ERP.py
+++ ERP.py
@@ -1,6 +1,7 @@
 #start a mini project console ERP system.
 employees={}
 teams={}
+org={}
 def main_menu():
 	print("Press 1 for add employee")
 	print("Press 2 for delete employee ")
@@ -8,18 +9,18 @@ def main_menu():
 	print("Press 4 for display  employee")
 	print("Press 5 for change employee details")
 	print("press 6 for manage all team")
-	print("Press 7 for Quit")
+	print("Press 7 for exit from this loop")
 
 def add_employee():
-	employee_id = int(input("\tenter Employee id "))
+	employee_id = int(input("\tenter Employee id: "))
 	if employee_id not in employees.keys():
-		name = input("\tenter name ")
+		name = input("\tenter name: ")
 		age = int(input("\tenter age "))
-		gender = input("\tenter the gender ")
+		gender = input("\tenter the gender: ")
 		place = input("\tenter place ")
-		salary = int(input("\tenter salary "))
-		previous_company = input("\tenter your previous company ")
-		joining_date = input("\tenter joining date ")
+		salary = int(input("\tenter salary :"))
+		previous_company = input("\tenter your previous company :")
+		joining_date = input("\tenter joining date: ")
 		temp ={
 			"name":name,
 			"age":age,
@@ -34,22 +35,63 @@ def add_employee():
 			print("\tEmployee id  already Taken")
 
 def delete_employee():
-	eid = int(input("\tEnter employee id"))
+	eid = int(input("\tEnter employee id: "))
 	if eid not in employees.keys():
-		print("\tWrong Employee id")
+		print("\tWrong Employee id: ")
 	else:
 		del employees[eid]
 
+#def search_employee():
+#	name = input("\tEnter name")
+#	found = False
+#	for i in employees.values():
+#		if i["name"] == name: 
+#			print(f"\t{i['name']} | {i['age']} | {i['place']} | {i['gender']} | {i['previous_company']} | {i['salary']} ")
+#			found = True
+#			break
+#		if found==False:
+#			print("\tnot in the list")
 def search_employee():
-	name = input("\tEnter name")
-	found = False
-	for i in employees.values():
-		if i["name"] == name: 
-			print(f"\t{i['name']} | {i['age']} | {i['place']} | {i['gender']} | {i['previous_company']} | {i['salary']} ")
-			found = True
+	
+	print("Press 1 for search by name")
+	print("Press 2 for search by age")
+	print("Press 3 for search by salary")
+	print("Press 4 for search  by gender")
+	print("Press 5 for exit")
+	while True:
+	
+		choice = int(input("Enter choice : "))
+		if choice == 1:
+			search_by_name()
+		elif choice == 2:
+			search_by_age()
+		elif choice == 3:
+			search_by_salary()
+		elif choice == 4:
+			search_by_gender()
+		elif choice == 5:
 			break
-		if found==False:
-			print("\tnot in the list")
+		else:
+			print("Invalid choice")
+def search_by_name():	
+	name=input("Enter the name: ")
+	print(list(filter(lambda a:a["name"] == name, employees.values())))
+	print("We Found")
+
+def search_by_age():	
+	age=int(input("Enter the age : "))
+	print(list(filter(lambda a:a["age"] == age, employees.values())))
+	print("We Found")
+
+def search_by_salary():	
+	salary=int(input("Enter the salary : "))
+	print(list(filter(lambda a:a["salary"] == salary, employees.values())))
+	print("We Found")
+
+def search_by_gender():	
+	gender=input("Enter the gender : ")
+	print(list(filter(lambda a:a["gender"] == gender, employees.values())))
+	print("We Found")
 
 def display_employee():
 	for id,employee in employees.items():
@@ -60,16 +102,16 @@ def change_employee():
 	print("Enter 2 for change age")
 	print("Enter 3 for change gender")
 	print("Enter 4 for change salary")
-	cho =int(input("\tEnter your choice."))
-	employee_id = input("\tEnter employee id.")
+	cho =int(input("\tEnter your choice : "))
+	employee_id = input("\tEnter employee id : ")
 	if cho == 1:
-		employees[employee_id]['name'] = input("\tEnter new name") 
+		employees[employee_id]['name'] = input("\tEnter new name: ") 
 	elif cho == 2:
-		employees[employee_id]['age'] = int(input("\tEnter new age"))
+		employees[employee_id]['age'] = int(input("\tEnter new age: "))
 	elif cho == 3:
-		employees[employee_id]['gender'] = input("\tEnter gender")
+		employees[employee_id]['gender'] = input("\tEnter gender: ")
 	elif cho == 4:
-		employees[employee_id]['salary'] = int(input("\tEnter new salary"))
+		employees[employee_id]['salary'] = int(input("\tEnter new salary: "))
 	else:
 		print("invalid")
 def manage_all_team_menu():
@@ -81,7 +123,7 @@ def manage_all_team_menu():
 def manage_all_team():
 	while True:
 		manage_all_team_menu()
-		ch=int(input("enter your choice"))
+		ch=int(input("enter your choice : "))
 		if ch == 1:
 		#create team
                 	create_team()
@@ -100,7 +142,7 @@ def manage_all_team():
 		else:
 			print("invalid option")
 def create_team():
-	team_name=input("\tEnter team name")
+	team_name=input("\tEnter team name : ")
 	teams[team_name]=[]
 def display_team():
 	for key,value in teams.items(): 
@@ -123,7 +165,7 @@ def manage_team_menu():
 	print("\t press 4. Exit")
 def manage_team():
 	while True:
-		team_name=input("\t\t Enter team name ")
+		team_name=input("\t\t Enter team name : ")
 		manage_team_menu()
 		ch=int(input("enter your choice"))
 		if ch == 1:
@@ -147,6 +189,7 @@ def add_member(team_name):
 		teams[team_name].append(eid)
 	else:
 		print("\t\t wrong eid")
+
 def delete_member(team_name):
 	display_member(team_name)
 	employee_id=int(input("\t\tEnter the employee id "))
@@ -154,16 +197,54 @@ def delete_member(team_name):
 		teams[team_name].remove(employee_id)
 	else:
 		print("\t\twrong id")
+
 def display_member(team_name):
 	name_string=""
 	for i in teams[team_name]:
 		name_string=name_string +"|"+i+"."+employee[i]["name"]
-	print(f"{name_string}")	
+	print(f"{name_string}")
+
+def add_oganization():
+	org['name']=input("enter organization name")
+	org['email']=input("enter organization  email")
+
+def edit_oganization():
+	print("1 for Edit Organization name ")
+	print("2 for Edit Organization email")
+	choice = int(input("Enter choice: "))
+	if choice == 1:
+		org['name'] = input("enter new organization name")
+	elif choice == 2:
+		org['email'] = input("enter new organization email")	
+	else:
+		print("invalid choice")
+def display_organization():
+	print(org)
+def org_details():
+	print("enter 1 for Add Organization")
+	print("enter 2 for Edit Organization")
+	print("enter 3 Display Organization details")
+	print("enter 4 for exit")
+	while True:
+	
+		choice = int(input("Enter choice : "))
+		if choice == 1:
+			add_oganization()
+		elif choice == 2:
+			edit_oganization()
+		elif choice == 3:
+			display_organization()
+		elif choice == 4:
+			break
+		else:
+			print("Invalid choice")	
 
 
 while True:
+	org_details()
+	#first organization details 
 	main_menu()
-	ch=int(input("enter your choice"))
+	ch=int(input("enter your choice: "))
 	if ch == 1:
 	#adding employee
 		add_employee()
